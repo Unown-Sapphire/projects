7fc02dc055149ee7eee2201a65d426611883ede4
UnoSapphire <72756485+Unown-Sapphire@users.noreply.github.com>
2023-12-23 01:12:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import clashroyale
 import requests
 import tkinter
+import pandas as pd
 
 token = clashroyale.official_api.Client("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjJkNDY4MmZjLWJmNGQtNDY5ZC04NDhlLWU2NTE3NjhmMjlkNiIsImlhdCI6MTcwMzE4NTExNSwic3ViIjoiZGV2ZWxvcGVyLzhkZWI0YWEwLWU5YjgtYzM4Mi01MmZjLWQ3NTIxM2MyODI1MSIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyIyMjMuMjMzLjgwLjE3MiJdLCJ0eXBlIjoiY2xpZW50In1dfQ.9w7Ux_eLODqg_ln6n9z9CvfQaILOYXRe5Y3qy_ojjpNYl7-lNL8lhtDJ_5A-JR3ySCJEmuTyBfdTxicGJX1dcw")
 
@@ -19,11 +20,13 @@ def player_info():
     player_name = player_json['name']
     player_wins = player_json['wins']
     player_3_crownwins = player_json["threeCrownWins"]
-    player_claninfo = player_json['clan']
-    print(player_claninfo['name'])
+    player_clan = player_json['clan']
+    print(f"Player's name: {player_name}")
+    print(f"Wins: {player_wins}")
+    print(f"3 Crown wins: {player_3_crownwins}")
+    print(f"Player's clan: {player_clan['name']}")
+    print(f"Player's clan role: {player_json['role']}")
 
-    print(player_name)
-    
 
 def chest():
     chest_url = f"https://api.clashroyale.com/v1/players/%23{player_tag}/upcomingchests"
@@ -32,10 +35,24 @@ def chest():
 
     chest_json_items = chest_json["items"]
     chest_list = list(chest_json_items[0:])
-    chest_name = chest_list[0:]
 
-    for i in chest_name:
-        print(f'{i["index"]} {i["name"]}')
+    print(chest_list)
+
+    chestNumber = []
+    upcomingChest = []
+
+    for i in chest_list:
+        chest_number = i['index']
+        upcoming_chest = i['name']
+        chestNumber.append(chest_number)
+        upcomingChest.append(upcoming_chest)
+
+    chest_dict = {
+        'chest_number' : chestNumber,
+        'name_chest' : upcomingChest
+    }      
+    df = pd.DataFrame(data=chest_dict)
+    print(df)
 
 player_info()
 chest()
